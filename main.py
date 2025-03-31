82254cab0472e0936c441dfe0e92868599dbcdee
BROKEN-PRINCE <sonam952567@gmail.com>
2025-03-31 11:08:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -102,4 +102,4 @@ def unlock_group():
         return jsonify({"status": "error", "message": "Unauthorized: Invalid token or UID."})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000
+    app.run(host='0.0.0.0', port=5000)
